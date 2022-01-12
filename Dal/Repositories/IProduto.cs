1396ab8a694a1fa1fcd3b59004a5c964c7ea032e
Gabriel Schincariol <gabriel.schincariol@iv2.com.br>
2022-01-12 20:35:56 -0300

    Interface Produto

---
+++ Dal/Repositories/IProduto.cs
@@ -0,0 +1,11 @@
+﻿namespace PetDelivey.WebAPI.Dal.Repositories
+{
+    public interface IProduto
+    {
+        Task<List<Model.Produto>> GetProdutos();
+        string AddProduto(Model.Produto produto);
+        void UpdateProduto(Model.Produto produto);
+        Task<Model.Produto> GetProdutoId(string id);
+        void DeleteProduto(string id);
+    }
+}
