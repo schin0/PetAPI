d83592a17513cb5d0bbcfc985e16352a78277640
Gabriel Schincariol <gabriel.schincariol@iv2.com.br>
2022-01-12 20:36:05 -0300

    Dal Produtos

---
+++ Dal/Produto.cs
@@ -0,0 +1,120 @@
+﻿using Google.Cloud.Firestore;
+using Newtonsoft.Json;
+using PetDelivey.WebAPI.Dal.Repositories;
+
+namespace PetDelivey.WebAPI.Dal
+{
+    public class Produto : IProduto
+    {
+        string projectId;
+        FirestoreDb fireStoreDb;
+
+        public Produto()
+        {
+            string arquivoApiKey = @"petdeliveryapi-firebase-adminsdk-3ddxj-215c84fd3e.json";
+            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", arquivoApiKey);
+            projectId = "nodeprojectimersao";
+            fireStoreDb = FirestoreDb.Create(projectId);
+        }
+
+        public string AddProduct(Model.Produto produto)
+        {
+            try
+            {
+                CollectionReference colRef = fireStoreDb.Collection("produtos");
+                var id = colRef.AddAsync(produto).Result.Id;
+                var shardRef = colRef.Document(id.ToString());
+                shardRef.UpdateAsync("Id", id);
+                return id;
+            }
+            catch
+            {
+                return "Error";
+            }
+        }
+
+        public string AddProduto(Model.Produto produto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async void DeleteProduto(string id)
+        {
+            try
+            {
+                DocumentReference produtoRef = fireStoreDb.Collection("produtos").Document(id);
+                await produtoRef.DeleteAsync();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public async Task<Model.Produto> GetProdutoId(string id)
+        {
+            try
+            {
+                DocumentReference docRef = fireStoreDb.Collection("produtos").Document(id);
+                DocumentSnapshot snapshot = await docRef.GetSnapshotAsync();
+                if (snapshot.Exists)
+                {
+                    Model.Produto produto = snapshot.ConvertTo<Model.Produto>();
+                    produto.Id = snapshot.Id;
+                    return produto;
+                }
+                else
+                {
+                    return new Model.Produto();
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public async Task<List<Model.Produto>> GetProdutos()
+        {
+            try
+            {
+                Query produtoQuery = fireStoreDb.Collection("produtos");
+                QuerySnapshot inscricaoQuerySnaphot = await produtoQuery.GetSnapshotAsync();
+                List<Model.Produto> listaProdutos = new List<Model.Produto>();
+                foreach (DocumentSnapshot documentSnapshot in inscricaoQuerySnaphot.Documents)
+                {
+                    if (documentSnapshot.Exists)
+                    {
+                        Dictionary<string, object> city = documentSnapshot.ToDictionary();
+                        string json = JsonConvert.SerializeObject(city);
+                        Model.Produto novoProduto = JsonConvert.DeserializeObject<Model.Produto>(json);
+                        novoProduto.Id = documentSnapshot.Id;
+                        listaProdutos.Add(novoProduto);
+                    }
+                }
+                List<Model.Produto> listaProdutoOrdenada = listaProdutos.OrderBy(x => x.Name).ToList();
+                return listaProdutoOrdenada;
+            }
+            catch (Exception ex)
+            {
+                var erro = ex.Message;
+                throw;
+            }
+        }
+
+        public async void UpdateProduto(Model.Produto produto)
+        {
+            try
+            {
+                DocumentReference produtoRef = fireStoreDb.Collection("produtos").Document(produto.Id);
+                await produtoRef.SetAsync(produto, SetOptions.Overwrite);
+            }
+            catch
+            {
+                throw;
+            }
+
+        }
+
+    }
+}
