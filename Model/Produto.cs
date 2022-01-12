91cb867750f7798c81c0d361557395767e759d47
Gabriel Schincariol <gabriel.schincariol@iv2.com.br>
2022-01-12 20:36:18 -0300

    Model de Produto

---
+++ Model/Produto.cs
@@ -0,0 +1,26 @@
+﻿using Google.Cloud.Firestore;
+
+namespace PetDelivey.WebAPI.Model
+{
+    [FirestoreData]
+    public class Produto
+    {
+        [FirestoreProperty]
+        public string Id { get; set; }
+
+        [FirestoreProperty]
+        public string Name { get; set; }
+
+        [FirestoreProperty]
+        public double Price { get; set; }
+
+        [FirestoreProperty]
+        public string urlImg { get; set; }
+
+        [FirestoreProperty]
+        public string descricaoImg { get; set; }
+
+        [FirestoreProperty]
+        public string descricaoProduto { get; set; }
+    }
+}
