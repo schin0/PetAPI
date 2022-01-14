e3f43bfcd3c776968a73a7dba1c362edd398d2db
Gabriel Schincariol <gabriel.schincariol@iv2.com.br>
2022-01-13 21:16:23 -0300

    Mudanças no Controller

--- Controllers/ProdutoController.cs
+++ Controllers/ProdutoController.cs
@@ -19,22 +19,18 @@ namespace PetDelivey.WebAPI.Controllers
         {
             return _produto.GetProdutos();
         }
-
         [HttpGet("{id}")]
         public Task<Model.Produto> Get(string id)
         {
             return _produto.GetProdutoId(id);
         }
-
         [HttpPost]
         public string Post([FromBody] Model.Produto produto) => _produto.AddProduto(produto);
-
         [HttpPut]
         public void Put([FromBody] Model.Produto produto)
         {
             _produto.UpdateProduto(produto);
         }
-
         [HttpDelete("{id}")]
         public void Delete(string id)
         {
